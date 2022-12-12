735ced3a7a1532e0559a34564b5af9a610ab6076
Jeliiiii <elacroix@gaming.tech>
2022-12-12 23:20:59 +0100

    maj

--- frontend/myapp/src/api/dashboardapi.js
+++ frontend/myapp/src/api/dashboardapi.js
@@ -1,3 +1,5 @@
+import { Createpoke } from "../components/dashboardpoke";
+
 export const getAll = async () => {
     const response = await fetch(
         'http://localhost:4444/pokemon/get', {
@@ -47,7 +49,7 @@ export const deletepokemonDash = async (pokemon) => {
     return pokemons
 }
 
-export const postpokemonDash = async (pokemon) => {
+export const postpokemonDash = async (Createpoke) => {
     const response = await fetch(
         'http://localhost:4444/pokemon/post', {
         method: 'POST',
@@ -56,15 +58,16 @@ export const postpokemonDash = async (pokemon) => {
             'Content-Type': 'application/json'
         }
         , body: JSON.stringify({
-            'num': pokemon.num,
-            'name': pokemon.name,
-            'type': pokemon.type,
-            'img1': pokemon.img1,
-            'img2': pokemon.img2,
-            'description': pokemon.description
+            'num': Createpoke.num,
+            'name': Createpoke.name,
+            'type': Createpoke.type,
+            'img1': Createpoke.img1,
+            'img2': Createpoke.img2,
+            'description': Createpoke.description
         })
     }
     )
     const pokemons = await response.json()
     return pokemons
 }
+
