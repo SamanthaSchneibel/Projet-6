842bdc45c37c6d6e5a55ce6652b5583e0a3baa90
Samantha <sschneibel@gaming.tech>
2022-12-12 21:42:41 +0100

    css

--- frontend/myapp/src/components/navbar.js
+++ frontend/myapp/src/components/navbar.js
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
  
 function Navbar(){
-    return <nav id="nav-wrapper" class="grey darken-4">
-            <a><Link to="/" class="brand-logo center">Attrappez les tous !</Link></a>
-            <ul class="left">
-                <li><Link to="/Pokedex">Pokedex</Link></li>
-            </ul>
-            <ul class="right">
-                <li><Link to="/Dashboard">Dashboard</Link></li>
-            </ul>
-        </nav>
+    return <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+      <a><Link to="/" class="navbar-brand">Attrappez les tous !</Link></a>
+      <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a><Link to="/Pokedex" class="nav-link left">Pokedex</Link></a>
+          <a><Link to="/Dashboard" class="nav-link right">Dashboard</Link></a>
+        </div>
+      </div>
+    </div>
+  </nav>
 }
 export default Navbar;
