ebd7c6112a24634945c66d2a071df65e21f03cd7
Sam <sschneibel@gaming.tech>
2022-12-12 14:37:46 +0100

    csss

--- frontend/myapp/src/components/navbar.js
+++ frontend/myapp/src/components/navbar.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
  
 function Navbar(){
-    return <nav>
-        <ul>
-            <li><Link to="/">Accueil</Link></li>
-            <li><Link to="/Pokedex">Pokedex</Link></li>
-            <li><Link to="/Dashboard">Dashboard</Link></li>
-        </ul>
+    return <nav class="grey darken-4">
+            <a href="#!" class="brand-logo center">Attrappez les tous !</a>
+            <ul class="left hide-on-med-and-down">
+                <li><Link to="/">Accueil</Link></li>
+                <li><Link to="/Pokedex">Pokedex</Link></li>
+                <li><Link to="/Dashboard">Dashboard</Link></li>
+            </ul>
     </nav>
 }
 export default Navbar;
