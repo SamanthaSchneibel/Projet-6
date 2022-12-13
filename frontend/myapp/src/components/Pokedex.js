caaaf1047f2b01c586944336fe4f4c93d8fb80ff
Samantha <sschneibel@gaming.tech>
2022-12-13 20:03:51 +0100

    css

--- frontend/myapp/src/components/Pokedex.js
+++ frontend/myapp/src/components/Pokedex.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { addedpokemon, updatepokemon, deletepokemon } from "../api/pokedexapi";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Pokedex() {
     const [pokemons, setPokemons] = useState([]);
@@ -9,38 +13,19 @@ function Pokedex() {
             .then(result => setPokemons(result))
             .catch(error => console.error("Erreur avec notre API :", error.message));
     }, []);
-    return <div className="row text-center">
-        <div className="div1 col-lg-3">
-            <div className="flex"> {
+    return <Container>
+        <h1>Vos pokemons dans le pc</h1>
+        <img className="background-pc" src="https://pixelmongenerations.com/wiki/images/9/9e/PC_Background_Forest.png"></img>
+        <Row className="div2 justify-content-md-center">
+            {
                 pokemons.map((pokemon, key) => {
-                    return <div key={key} className="bloc-pokedex">
-                        <div className="background-avatar">
-                            <img className="avatar" src={pokemon.img1} alt="" />
-                        </div>
-                        <div className="infos">
-                            <h3><br />{pokemon.name}{pokemon.num}</h3>
-                            <img className="type" src={pokemon.type} alt="" />
-                            <img className="type2" src={pokemon.type2} alt="" />
-                        </div>
-                        <p><br />{pokemon.description}<br /></p>
-                        <input type="text" id={key} name="Nom" />
-                        <button type="button" className="btn btn-outline-dark" onClick={() => updatepokemon(pokemon, key)}>Renommer</button>
-                        <button type="button" className="btn btn-outline-dark" onClick={() => deletepokemon(pokemon)}>Relâcher</button>
+                    return <Col key={key} xs={{ order: 1 }}><div className="bloc-pokedex">
+                        <img type="button" className="avatar-pixel" src={pokemon.img2} alt="" />
                     </div>
+                    </Col>
                 })}
-            </div>
-        </div>
-        <div className="div2 col-lg-6">
-                <h1>Vos pokemons dans le pc</h1>
-                <div className="pixel"> {
-                    pokemons.map((pokemon, key) => {
-                        return <div key={key} className="bloc-pokedex">
-                            <img type="button" className="avatar-pixel" src={pokemon.img2} alt="" />
-                        </div>
-                    })}
-                </div>
-        </div>
-    </div >;
+        </Row>
+    </Container>
 };
 
 export default Pokedex;
--- frontend/myapp/src/components/PokemonHome.js
+++ frontend/myapp/src/components/PokemonHome.js
@@ -15,19 +15,19 @@ function PokemonHome() {
             .catch(error => console.error("Erreur avec notre API :", error.message));
     }, []);
     return <Container>
-            <h1>Capturez les pokemons sauvages</h1>
-            <Row className="justify-content-md-center">
-                {
-                    pokemons.map((pokemon, key) => {
-                        return <Col key={key}  xs={12} md={6} lg={4}><div className="bloc-pokemon">
-                            <img className="avatar" src={pokemon.img1} alt="" />
-                            <h2>{pokemon.name}</h2>
-                            <button type="button" class="btn" onClick={() => addToPokedex(pokemon)}><img className="pokeball" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/769px-Pokebola-pokeball-png-0.png" /></button>
-                            </div>
-                        </Col>
-                    })}
-            </Row>
-        </Container>
+        <h1>Capturez les pokemons sauvages</h1>
+        <Row className="justify-content-md-center">
+            {
+                pokemons.map((pokemon, key) => {
+                    return <Col key={key} xs={12} md={6} lg={4}><div className="bloc-pokemon">
+                        <img className="avatar" src={pokemon.img1} alt="" />
+                        <h2>{pokemon.name}</h2>
+                        <button type="button" class="btn" onClick={() => addToPokedex(pokemon)}><img className="pokeball" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/769px-Pokebola-pokeball-png-0.png" /></button>
+                    </div>
+                    </Col>
+                })}
+        </Row>
+    </Container>
 };
 
 export default PokemonHome;
