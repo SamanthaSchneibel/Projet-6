b50be38aa59c3e0e8fc155b80e3ea5a3e2ca9676
Samantha <sschneibel@gaming.tech>
2022-12-13 12:07:16 +0100

    css

--- frontend/myapp/src/components/Pokedex.js
+++ frontend/myapp/src/components/Pokedex.js
@@ -9,18 +9,24 @@ function Pokedex() {
             .then(result => setPokemons(result))
             .catch(error => console.error("Erreur avec notre API :", error.message));
     }, []);
-    return <div className="pokemon-list">
-        <img className="background-pc" src="https://pixelmongenerations.com/wiki/images/9/9e/PC_Background_Forest.png"></img>
+    return <div className="pokedex-list">
+            <img className="background" src="https://pixelmongenerations.com/wiki/images/9/9e/PC_Background_Forest.png"></img>
         <div className="flex"> {
             pokemons.map((pokemon, key) => {
                 return <div key={key} className="bloc-pokedex">
-                    <img className="avatar" src={pokemon.img1} alt="" />
-                    <img className="avatar-pixel" src={pokemon.img2} alt="" />
-                    <img className="type" src={pokemon.type} alt="" />
-                    <h2>{pokemon._id}<br/>Numéro du pokedex: {pokemon.num} <br/>Nom: {pokemon.name} <br/> Description: {pokemon.description}</h2>
-                    <input type="text" id={key} name="Nom" required className="required text"/>
-                    <button onClick={() => updatepokemon(pokemon, key)}>Donner un surnom:</button>
-                    <button onClick={() => deletepokemon(pokemon)}>Supprimer le pokemon:</button>
+                    <div className="infospkm">
+                        <div className="background-avatar">
+                            <img className="avatar" src={pokemon.img1} alt="" />
+                        </div>
+                        <div className="background-other">
+                            <img className="type" src={pokemon.type} alt="" />
+                            <h2>{pokemon._id}<br/>Numéro du pokedex: {pokemon.num} <br/>Nom: {pokemon.name} <br/> Description: {pokemon.description}</h2>
+                            <input type="text" id={key} name="Nom" required className="required text"/>
+                            <button type="button" class="btn btn-outline-danger" onClick={() => updatepokemon(pokemon, key)}>Renommer</button>
+                            <button type="button" class="btn btn-outline-danger" onClick={() => deletepokemon(pokemon)}>Relâcher</button>
+                        </div>
+                    </div>
+                    <img type="button" className="avatar-pixel" src={pokemon.img2} alt="" />   
                 </div>
             })}
         </div>
