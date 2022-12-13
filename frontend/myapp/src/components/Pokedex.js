5b47745dea0da724590749912c294dfa0f5075a6
Samantha <sschneibel@gaming.tech>
2022-12-13 14:04:07 +0100

    css

--- frontend/myapp/src/components/Pokedex.js
+++ frontend/myapp/src/components/Pokedex.js
@@ -10,7 +10,7 @@ function Pokedex() {
             .catch(error => console.error("Erreur avec notre API :", error.message));
     }, []);
     return <div className="pokedex-list">
-            <img className="background" src="https://pixelmongenerations.com/wiki/images/9/9e/PC_Background_Forest.png"></img>
+            <img className="container" src="https://pixelmongenerations.com/wiki/images/9/9e/PC_Background_Forest.png"></img>
         <div className="flex"> {
             pokemons.map((pokemon, key) => {
                 return <div key={key} className="bloc-pokedex">
@@ -18,7 +18,7 @@ function Pokedex() {
                         <div className="popup bg-light">
                             <img className="avatar" src={pokemon.img1} alt="" />
                             <img className="type" src={pokemon.type} alt="" />
-                            <h2>{pokemon._id}<br/>Numéro du pokedex: {pokemon.num} <br/>Nom: {pokemon.name} <br/> Description: {pokemon.description}</h2>
+                            <h3>{pokemon._id}<br/>Numéro du pokedex: {pokemon.num} <br/>Nom: {pokemon.name} <br/> Description: {pokemon.description}</h3>
                             <input type="text" id={key} name="Nom" required className="required text"/>
                             <button type="button" class="btn btn-outline-danger" onClick={() => updatepokemon(pokemon, key)}>Renommer</button>
                             <button type="button" class="btn btn-outline-danger" onClick={() => deletepokemon(pokemon)}>Relâcher</button>
