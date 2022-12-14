f5f4a7149e7e8c34871deef80a34338589eca217
Samantha <sschneibel@gaming.tech>
2022-12-14 08:26:11 +0100

    css

--- frontend/myapp/src/components/navbar.js
+++ frontend/myapp/src/components/navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
       <div className="nav-bar">
         <ul>
           <li><Link to="/Pokedex">Pokedex</Link></li>
-          <li><Link to="/"><img src="https://www.pngkit.com/png/full/2-20874_pokemon-logo-pokemon-black-and-white-logo.png"/></Link></li>
+          <li><Link to="/"><img src="https://www.pngkit.com/png/full/2-20874_pokemon-logo-pokemon-black-and-white-logo.png" alt=""/></Link></li>
           <li><Link to="/Dashboard">Dashboard</Link></li>
         </ul>
       </div>
